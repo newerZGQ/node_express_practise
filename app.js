a4ca0f3e06da0db901a313e5b95d20cee3ec1d51
zgq_ubuntu <zgq2015@foxmail.com>
2019-10-09 09:39:14 +0800

    express

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var entries = require('./routes/entries');
 
 var app = express();
 
@@ -15,13 +16,14 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.get('/', entries.list);
 app.use('/users', usersRouter);
-
+app.get('/post', entries.form);
+app.post('/post', entries.form);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
