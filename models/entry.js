9ce21df222c2199d016519513cabea95d11335c8
nanwei.zgq <nanwei.zgq@alibaba-inc.com>
2020-07-14 11:33:23 +0800

    demo

--- models/entry.js
+++ models/entry.js
@@ -1,35 +1,35 @@
-const redis = require('redis')
-const db = redis.createClient()
+// const redis = require('redis')
+// const db = redis.createClient()
 
-class Entry {
-    constructor(obj){
-        for( let key in obj){
-            this[key] = obj[key];
-        }
-    }
+// class Entry {
+//     constructor(obj){
+//         for( let key in obj){
+//             this[key] = obj[key];
+//         }
+//     }
 
-    save(cb){
-        const entryJSON = JSON.stringify(this);
-        db.lpush(
-            'entries',
-            entryJSON,
-            (err) => {
-                if(err) return cb(err);
-                cb();
-            }
-        )
-    }
+//     save(cb){
+//         const entryJSON = JSON.stringify(this);
+//         db.lpush(
+//             'entries',
+//             entryJSON,
+//             (err) => {
+//                 if(err) return cb(err);
+//                 cb();
+//             }
+//         )
+//     }
 
-    static getRange(from, to, cb){
-        db.lrange('entries', from, to, (err, items) => {
-            if(err) return cb(err);
-            let entries = [];
-            items.forEach((item) => {
-                entries.push(JSON.stringify(item));
-            });
-            cb(null, entries);
-        })
-    }
-}
+//     static getRange(from, to, cb){
+//         db.lrange('entries', from, to, (err, items) => {
+//             if(err) return cb(err);
+//             let entries = [];
+//             items.forEach((item) => {
+//                 entries.push(JSON.stringify(item));
+//             });
+//             cb(null, entries);
+//         })
+//     }
+// }
 
-module.exports = Entry;
+// module.exports = Entry;
